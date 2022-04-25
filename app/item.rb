6b782ebece47e21dd8d65d8cfb2fb2427c989768
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-25 15:21:35 +0000

    Fix errors spotted by linters

--- app/item.rb
+++ app/item.rb
@@ -2,12 +2,14 @@ class Item
   attr_accessor :name, :publish_date
   attr_reader :id, :archived, :genre, :author, :label
 
+  # rubocop:disable Style/OptionalBooleanParameter
   def initialize(name, publish_date, archived = false, id = Random.rand(1..100_000))
     @id = id
     @name = name
     @publish_date = publish_date # Time.new(yyyy, mm, dd)
     @archived = archived
   end
+  # rubocop:enable Style/OptionalBooleanParameter
 
   def add_label=(label)
     @label = label
@@ -23,5 +25,4 @@ class Item
     @author = author
     author.items.push(self) unless author.items.include?(self)
   end
-  
 end
