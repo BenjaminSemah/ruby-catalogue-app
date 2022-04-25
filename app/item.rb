85efe7833a9993699bb760aefc6361cd76a98d17
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-25 14:40:52 +0000

    Add Item class

---
+++ app/item.rb
@@ -0,0 +1,27 @@
+class Item
+  attr_accessor :name, :publish_date
+  attr_reader :id, :archived, :genre, :author, :label
+
+  def initialize(name, publish_date, archived = false, id = Random.rand(1..100_000))
+    @id = id
+    @name = name
+    @publish_date = publish_date # Time.new(yyyy, mm, dd)
+    @archived = archived
+  end
+
+  def add_label=(label)
+    @label = label
+    label.items.push(self) unless label.items.include?(self)
+  end
+
+  def add_genre=(genre)
+    @genre = genre
+    genre.items.push(self) unless genre.items.include?(self)
+  end
+
+  def add_author=(author)
+    @author = author
+    author.items.push(self) unless author.items.include?(self)
+  end
+
+end
