3e1abbfba585967f738ba30fd93831fa0ac002d4
unknown <butlermuwo@gmail.com>
2022-04-25 17:56:12 +0200

    Add archive methods

--- app/item.rb
+++ app/item.rb
@@ -25,4 +25,14 @@ class Item
     @author = author
     author.items.push(self) unless author.items.include?(self)
   end
+
+  def move_to_archive
+    @archived = true if can_be_archived?
+  end
+
+  private
+
+  def can_be_archived?
+    @publish_date.year < Time.new.year - 10
+  end
 end
