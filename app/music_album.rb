21e053269c3653daff7aa5bf173598fb67ac5fc6
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-26 06:13:58 +0000

    Define MusicAlbum class properties

--- app/music_album.rb
+++ app/music_album.rb
@@ -1,5 +1,10 @@
 require_relative './item'
 
 class MusicAlbum < item
-  
+  attr_accessor :on_spotify
+
+  def initialize(name, publish_date, archived = false, on_spotify = false, id = Random.rand(1..100_000))
+    super(name, publish_date, archived, id)
+    @on_spotify = on_spotify
+  end
 end
