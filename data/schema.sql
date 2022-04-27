2e0e9792ed700e080e357085165f53e6662079bd
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-27 00:33:29 +0000

    Add schema for genres and music_albums

--- data/schema.sql
+++ data/schema.sql
@@ -1,1 +1,16 @@
+CREATE DATABASE catalogue;
 
+CREATE TABLE genres(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE music_albums(
+  id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(150) NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN,
+  on_spotify BOOLEAN NOT NULL,
+  genre_id INT,
+  FOREIGN KEY (genre_id) REFERENCES genre(id)
+);
