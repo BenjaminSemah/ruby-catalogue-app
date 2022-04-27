c52801354008db1ad767f103ab8533424bdad185
Benjamin Semah <benjaminsemah@gmail.com>
2022-04-27 09:24:30 +0000

    Fix errors spotted by linters

--- app/book.rb
+++ app/book.rb
@@ -4,12 +4,14 @@ class Book < Item
   attr_accessor :cover_state, :publisher
 
   # rubocop:disable Metrics/ParameterLists
-  def initialize(name, publish_date, publisher, cover_state, archived = false, id = Random.rand(1..100_000))
+  # rubocop:disable Style/OptionalBooleanParameter
+  def initialize(name, _publish_date, publisher, cover_state, archived = false, id = Random.rand(1..100_000))
     super(name, published_date, archived, id)
     @publisher = publisher
     @cover_state = cover_state
   end
   # rubocop:enable Metrics/ParameterLists
+  # rubocop:enable Style/OptionalBooleanParameter
 
   private
 
